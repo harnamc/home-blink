d04fa834d871b827e5ecdc8c19d790ffbfe7d7cf
Harnam Chana <harnamsc@amazon.co.uk>
2025-02-20 14:30:49 +0000

    Update logger to write to stdout

--- src/home_blink/logger.py
+++ src/home_blink/logger.py
@@ -1,13 +1,12 @@
 import logging
+import sys
 
 
 def get_logger(name: str):
     logging.basicConfig(
         level=logging.INFO,
         format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
-        handlers=[
-            logging.StreamHandler(),
-        ],
+        stream=sys.stdout,
     )
 
     logger = logging.getLogger(name)
